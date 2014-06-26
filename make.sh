ff86083068088739b5a9e3499df3e2439d4a5b5d
Sven Michael Klose <pixel@copei.de>
2014-06-26 19:28:05 +0200

    Split framework and application a bit.

--- make.sh
+++ make.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-cd client ; coffee -c -b -o ../js \
-    levenshtein.coffee \
-    types.coffee \
-    schemas.coffee \
-    xml-gen.coffee \
-    overlay.coffee \
-    create-input.coffee \
-    create-form.coffee \
-    preferences.coffee \
-    menu.coffee \
-    index.coffee
+coffee -c -b -o ../js client/*.coffee app/client/*.coffee
