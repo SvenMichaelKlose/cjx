535767219965681ffd1a25f72dced3c23d6cf2c7
Sven Michael Klose <pixel@copei.de>
2014-06-27 15:16:44 +0200

    Have a revision number.

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-for i in client/*.coffee app/*.coffee; do echo "Compiling $i..."; coffee -c -o js $i; done
+echo -n "@REVISION =" > _revision.coffee
+git log | grep Author: | wc -l >>_revision.coffee
+for i in _revision.coffee client/*.coffee app/*.coffee; do echo "Compiling $i..."; coffee -c -o js $i; done
