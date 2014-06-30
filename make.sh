8889bf19382041ca829ddea74be3cdd195841d77
Sven Michael Klose <pixel@copei.de>
2014-06-30 14:26:41 +0200

    Stop on errors.

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 
 echo -n "@REVISION =" > _revision.coffee
 git log | grep Author: | wc -l >>_revision.coffee
-for i in _revision.coffee client/*.coffee app/*.coffee; do echo "Compiling $i..."; coffee -c -o js $i; done
+for i in _revision.coffee client/*.coffee app/*.coffee; do echo "Compiling $i..."; coffee -c -o js $i || exit 1; done
