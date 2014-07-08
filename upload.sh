e38dc7f21041138fd89ab5714733fc9d68f39746
Sven Michael Klose <pixel@copei.de>
2014-07-08 17:11:16 +0200

    Only copy compiled files.

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-scp -rC css js index.html copei.de:/wwwroot/copei.de/www/pi/admin/
+#scp -rC css js index.html copei.de:/wwwroot/copei.de/www/pi/admin/
+scp -C js/* copei.de:/wwwroot/copei.de/www/pi/admin/js/
