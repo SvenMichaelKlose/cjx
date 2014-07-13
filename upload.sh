d9b7c430f7eab669246045116901beb12e88c01a
Sven Michael Klose <pixel@copei.de>
2014-07-13 18:45:31 +0200

    Have PHP script in root, so the whole thing can by synced with the web server directory easier.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 #scp -rC css js index.html copei.de:/wwwroot/copei.de/www/pi/admin/
-rsync -av --rsh=ssh js copei.de:/wwwroot/copei.de/www/pi/admin/
-rsync -av --rsh=ssh index.html copei.de:/wwwroot/copei.de/www/pi/admin/
+rsync -av --rsh=ssh . copei.de:/wwwroot/copei.de/www/pi/admin/
