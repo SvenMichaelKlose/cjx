11c8a654195b9013d9b146e0d47ccd62b8ebac0d
Sven Michael Klose <pixel@copei.de>
2014-07-19 17:03:56 +0200

    Skip file with no name.

--- upload.php
+++ upload.php
@@ -3,6 +3,8 @@ $FILES_ROOT = "/wwwroot/copei.de/www/pi/files/";
 echo "<textarea data-type=\"text/xml\"><files>";
 for ($i = 0; $i < sizeof ($_FILES["file"]["name"]); $i++) {
   $name = $_FILES["file"]["name"][$i];
+  if (!$name)
+    continue;
   $path = $FILES_ROOT . $name;
   move_uploaded_file ($_FILES["file"]["tmp_name"][$i], $path);
   echo "<file src=\"http://copei.de/pi/files/$name\"/>";
